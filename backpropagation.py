b6620e8ddb610b725f50371557a8ea432e09a9f2
Vladimir Ilyin <vladimir.ilyin@firstlinesoftware.com>
2018-02-23 23:06:37 +0300

    Added a plot of errors. Added E and alpha hyper parameters. Removed initial comments.

--- backpropagation.py
+++ backpropagation.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def nonlin(x, deriv=False):
@@ -18,38 +19,53 @@ y = np.array([[0],
               [1],
               [0]])
 
-np.random.seed(1)
+E = 0.7
+alpha = 0.3
 
-# случайно инициализируем веса, в среднем - 0
+#np.random.seed(1)
+
 syn0 = 2 * np.random.random((3, 4)) - 1
 syn1 = 2 * np.random.random((4, 1)) - 1
 
-for j in xrange(60000):
+prev_syn0_delta = np.array([[0, 0, 0, 0],
+                            [0, 0, 0, 0],
+                            [0, 0, 0, 0]])
 
-    # проходим вперёд по слоям 0, 1 и 2
-    l0 = X
-    l1 = nonlin(np.dot(l0, syn0))
-    l2 = nonlin(np.dot(l1, syn1))
+prev_syn1_delta = np.array([[0],
+                            [0],
+                            [0],
+                            [0]])
 
-    # как сильно мы ошиблись относительно нужной величины?
-    l2_error = y - l2
+epoch_count = 60000
 
-    if (j % 5000) == 0:
-        print "Error:" + str(np.mean(np.abs(l2_error)))
+errors = np.zeros(epoch_count);
 
-    # в какую сторону нужно двигаться?
-    # если мы были уверены в предсказании, то сильно менять его не надо
-    l2_delta = l2_error * nonlin(l2, deriv=True)
+for j in xrange(epoch_count):
 
-    # как сильно значения l1 влияют на ошибки в l2?
-    l1_error = l2_delta.dot(syn1.T)
+    l0 = X # 4x3
+    l1 = nonlin(np.dot(l0, syn0)) # 4x3 * 3x4 = 4x4
+    l2 = nonlin(np.dot(l1, syn1)) # 4x4 * 4x1 = 4x1
 
-    # в каком направлении нужно двигаться, чтобы прийти к l1?
-    # если мы были уверены в предсказании, то сильно менять его не надо
-    l1_delta = l1_error * nonlin(l1, deriv=True)
+    l2_error = y - l2 # 4x1 - 4x1
 
-    grad1 = l1.T.dot(l2_delta)
-    grad2 = l0.T.dot(l1_delta)
+    error = np.mean(np.abs(l2_error))
+    errors[j] = error
 
-    syn1 += grad1
-    syn0 += grad2
+    l2_delta = l2_error * nonlin(l2, deriv=True) # 4x1 * 4x1 = 4x1
+
+    l1_error = l2_delta.dot(syn1.T) # 4x1 * 1x4 = 4x4
+
+    l1_delta = l1_error * nonlin(l1, deriv=True) # 4x4 * 4x4
+
+    grad1 = l1.T.dot(l2_delta) # 4x4 * 4x1 = 4x1
+    grad2 = l0.T.dot(l1_delta) # 3x4 * 4x4 = 3x4
+
+    syn0_delta = E * grad2 + alpha * prev_syn0_delta
+    syn1_delta = E * grad1 + alpha * prev_syn1_delta
+    prev_syn1_delta = syn1_delta
+
+    syn1 += syn1_delta
+    syn0 += syn0_delta
+
+plt.plot(errors)
+plt.show()
